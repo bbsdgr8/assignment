410bc393976f634f2232cc9d2926d80e9aa9d118
rpi-roarellora <rpi-roarellora@403DWXN271.starhubsg.sh.inc>
2019-11-21 16:03:55 +0800

    test

--- weather-app/src/test/java/weather/ApplicationTest.java
+++ weather-app/src/test/java/weather/ApplicationTest.java
@@ -19,15 +19,18 @@ package weather;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import weather.controller.UIController;
+import weather.service.DarkSkyAPIService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = UIController.class)
@@ -36,10 +39,14 @@ public class ApplicationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    DarkSkyAPIService darkSkyAPIService;
+    
     @Test
     public void getWeather() throws Exception {
         mockMvc.perform(get("/weatherNow"))
-                .andExpect(content().string(containsString("Helloooo, World!")));
+        		.andExpect(status().isOk())
+                .andExpect(content().string(containsString("Weather Today")));
     }
 
 }
